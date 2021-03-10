596d2d0ce20fa19fa287640d993c38d899f1a66b
Cristina Lefemine <64276438+clefemine@users.noreply.github.com>
2021-03-09 20:09:06 -0800

    edit comments

--- Earthquake_Challenge/static/js/logic.js
+++ Earthquake_Challenge/static/js/logic.js
@@ -1,8 +1,8 @@
-// Add console.log to check to see if our code is working.
+// Add console.log to check code.
 console.log("working");
 
 
-// We create the tile layers that will be the background of our map.
+// tile layers that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
@@ -21,7 +21,7 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 	id: 'light-v10',
 	accessToken: API_KEY
 });
-// Create a base layer that holds both maps.
+// base layer that holds both maps.
 let baseMaps = {
 	"Streets": streets,
 	"Satellite": satelliteStreets,
@@ -35,7 +35,7 @@ let baseMaps = {
 	layers: [streets]
 })
 
-  // Create the earthquake layer for our map.
+  // earthquake layer for our map.
 let earthquakes = new L.layerGroup();
 let tectonicPlates = new L.LayerGroup();
 // We define an object that contains the overlays.
@@ -44,8 +44,7 @@ let overlays = {
 	"Earthquakes": earthquakes,
 	"Tectonic Plates": tectonicPlates
   };
-// Then we add a control to the map that will allow the user to change
-// which layers are visible.
+// add a control to the map that will allow the user to change
 L.control.layers(baseMaps, overlays).addTo(map);
 
 
@@ -70,7 +69,7 @@ function styleInfo(feature) {
 	  weight: 0.5
 	};
   }
-  // This function determines the color of the circle based on the magnitude of the earthquake.
+  // circle based on the magnitude of the earthquake.
 function getColor(magnitude) {
 	if (magnitude > 5) {
 	  return "#ea2c2c";
@@ -89,7 +88,7 @@ function getColor(magnitude) {
 	}
 	return "#98ee00";
   }
-  // This function determines the radius of the earthquake marker based on its magnitude.
+  //radius of the earthquake marker based on its magnitude.
 // Earthquakes with a magnitude of 0 will be plotted with a radius of 1.
 function getRadius(magnitude) {
 	if (magnitude === 0) {
@@ -100,15 +99,15 @@ function getRadius(magnitude) {
 // Creating a GeoJSON layer with the retrieved data.
 L.geoJson(data, {
 
-	// We turn each feature into a circleMarker on the map.
+	//  turn each feature into a circleMarker on the map.
 	
 	pointToLayer: function(feature, latlng) {
 				  console.log(data);
 				  return L.circleMarker(latlng);
 			},
-		  // We set the style for each circleMarker using our styleInfo function.
+		  // set the style for each circleMarker using our styleInfo function.
 		style: styleInfo,
-		// We create a popup for each circleMarker to display the magnitude and
+		// create a popup for each circleMarker to display the magnitude and
     //  location of the earthquake after the marker has been created and styled.
     onEachFeature: function(feature, layer) {
 		layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
@@ -117,7 +116,7 @@ L.geoJson(data, {
 	});
 
 
-// Then we add our 'graymap' tile layer to the map.
+// add our 'graymap' tile layer to the map.
 earthquakes.addTo(map);
 
 // create api link to tectonic plate data
@@ -130,7 +129,7 @@ let myStyle = {
 
 d3.json(geoData).then(function(geoData) {
 	console.log(geoData);
-  // Creating a GeoJSON layer with the retrieved data.
+  // GeoJSON layer with the retrieved data.
   L.geoJson(geoData, {
 	  style: myStyle,
     onEachFeature: function(feature, layer) {
@@ -139,12 +138,12 @@ d3.json(geoData).then(function(geoData) {
 }).addTo(tectonicPlates);
 });
 
-// Create a legend control object.
+// legend control object
 let legend = L.control({
 	position: "bottomright"
   });
   
-// Then add all the details for the legend.
+// add all the details for the legend
 legend.onAdd = function() {
 	let div = L.DomUtil.create("div", "info legend");
 	const magnitudes = [0, 1, 2, 3, 4, 5];
